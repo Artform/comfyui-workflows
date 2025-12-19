f0d8173b8ba876788ec73fad6b169c67801a4de9
Ahmed Sagarwala <am@artform.ca>
2025-12-19 15:07:22 -0500

    Cleanup
    Autoshow script running on Mac

--- cias.py
+++ cias.py
@@ -4,22 +4,14 @@ import uuid
 import json
 import urllib.request
 import urllib.parse
+import os
+import time
+from datetime import datetime
+import shutil
+import random
 
 server_address = "127.0.0.1:8188"
 client_id = str(uuid.uuid4())
-
-# Move file to input folder
-def move_file_to_input_folder(file_path, input_folder):
-    import shutil
-    import os
-
-    if not os.path.exists(input_folder):
-        os.makedirs(input_folder)
-
-    file_name = os.path.basename(file_path)
-    destination = os.path.join(input_folder, file_name)
-    shutil.move(file_path, destination)
-    return destination
 
 def queue_prompt(prompt, prompt_id):
     p = {"prompt": prompt, "client_id": client_id, "prompt_id": prompt_id}
@@ -37,35 +29,6 @@ def get_history(prompt_id):
     with urllib.request.urlopen("http://{}/history/{}".format(server_address, prompt_id)) as response:
         return json.loads(response.read())
 
-def get_images(ws, prompt):
-    prompt_id = str(uuid.uuid4())
-    queue_prompt(prompt, prompt_id)
-    output_images = {}
-    while True:
-        out = ws.recv()
-        if isinstance(out, str):
-            message = json.loads(out)
-            if message['type'] == 'executing':
-                data = message['data']
-                if data['node'] is None and data['prompt_id'] == prompt_id:
-                    break #Execution is done
-        else:
-            # If you want to be able to decode the binary stream for latent previews, here is how you can do it:
-            # bytesIO = BytesIO(out[8:])
-            # preview_image = Image.open(bytesIO) # This is your preview in PIL image format, store it in a global
-            continue #previews are binary data
-
-    history = get_history(prompt_id)[prompt_id]
-    for node_id in history['outputs']:
-        node_output = history['outputs'][node_id]
-        images_output = []
-        if 'images' in node_output:
-            for image in node_output['images']:
-                image_data = get_image(image['filename'], image['subfolder'], image['type'])
-                images_output.append(image_data)
-        output_images[node_id] = images_output
-
-    return output_images
 
 # Configure the prompt to run the workflow (latest file goes here)
 def queue_prompt(prompt):
@@ -74,6 +37,20 @@ def queue_prompt(prompt):
     req = urllib.request.Request(f"http://{server_address}/prompt", data=data)
     return json.loads(urllib.request.urlopen(req).read())
 
+def choicer(options):
+    return random.choice(options.split("|"))
+
+def prompt_creater():
+    car_type = "classic|widebody|concept|modern|low-rider|muscle|law enforcement|drone|futuristic|steampunk|cyberpunk|modded|street|military|offroad"
+    paint_type = "vibrant|iridescent|orange|gun metal|purple|blue|cyan|red|yellow|green|gold|silver|flames decal|platinum|metallic|matte|camo|neon LED accents"
+    background_type = "city|skyline|winter|jungle|slum|extraterrestrial|shipping yard|summer|tropical|alpine|Banff|Norway|Hawaii|cavernous|futuristic|urban|apocalyptic|California|warzone|Monaco|Greek|Roman|Caribbean|college campus|mountainous|rural|lunar|winding road|mansion|modern|cliffside|volcano"
+    time_of_day = "early morning|night time|evening|golden hour"
+    
+    prompt_text = f"professional photo of a {choicer(car_type)} car with {choicer(paint_type)} paint, metal geometric symmetrical rims on wheels, clean contours,{choicer(background_type)} background, {choicer(time_of_day)}, professional lighting, highly detailed, high budget,marketing photo, cinemascope, gorgeous, slick, masterpiece"
+    return prompt_text
+
+# ---------- Main code ----------
+
 # Define the prompt JSON using the existing workflow file
 with open("cias.json", "r", encoding="utf-8") as f:
     workflow_data = f.read()
@@ -81,30 +58,33 @@ with open("cias.json", "r", encoding="utf-8") as f:
 
 # Check if scan folder has a file
 folder = "/Users/razaa/Documents/ComfyUI/input/scan/"
-file = folder + "img20251202_17050626.png"  # Example file path
-prompt["102"]["inputs"]["image"] = file
-# Check if file was located
-if not file:
-    raise Exception("No file found in scan folder")
-else:
-    print(f"Found file: {file}")
-    prompt["102"]["inputs"]["image"] = file
-
-ws = websocket.WebSocket()
-ws.connect("ws://{}/ws?clientId={}".format(server_address, client_id))
-
-#images = get_images(ws, prompt)
-
-response = queue_prompt(prompt)
-print("Workflow queued, response:")
-print(response)
-
-ws.close() # for in case this example is used in an environment where it will be repeatedly called, like in a Gradio app. otherwise, you'll randomly receive connection timeouts
-#Commented out code to display the output images:
-
-# Run API call
+#file = folder + "img20251202_17050626.png"  # Example file path
 
 
+#loop to check for new files in the scan folder
+print(str(datetime.now().timestamp()) + f": Checking folder (ctrl+c to quit) {folder}")
+while True:
+    # delay = 10  # seconds
+    files = os.listdir(folder)
+    if files:
+        #loop through to find each file
+        for file in files:
+            #ensure it's an image file
+            if file.lower().endswith(('.png', '.jpg', '.jpeg')):
+                print(str(datetime.now().timestamp()) + f": New file found: scan/{file}")
+                # # Run the ComfyUI API call using websockets
+                ws = websocket.WebSocket()
+                ws.connect("ws://{}/ws?clientId={}".format(server_address, client_id))
+                
+                # Shift the processed file to the output folder
+                shutil.move(folder + file, "/Users/razaa/Documents/ComfyUI/input/scan/output/" + file)  # Move the file to the output subfolder
+                prompt["11"]["inputs"]["image"] = "scan/output/" + file
+                prompt["45"]["inputs"]["seed"] = random.randint(0, 2**32 - 1)  # New random seed for each run
+                prompt["7"]["inputs"]["text"] = prompt_creater()  # Generate a new prompt for each run
 
-
-#move_file_to_input_folder(file, "/Users/razaa/Documents/ComfyUI/input/")
+                response = queue_prompt(prompt)
+                print(str(datetime.now().timestamp()) + ": " + str(response))
+                ws.close() # for in case this example is used in an environment where it will be repeatedly called, like in a Gradio app. otherwise, you'll randomly receive connection timeouts
+                
+        # print(str(datetime.now().timestamp()) + f": Waiting {delay} for a change (ctrl+c to exit) - ")
+    # time.sleep(delay)  # Simulate processing time
